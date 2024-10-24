7cbd3abeeedfaf27056bacac5550393d74668445
evgeniy-fedorchenko <jecky432@gmail.com>
2024-10-24 22:05:32 +0500

    security: few edits in logging aspect for controller

--- src/main/java/com/efedorchenko/timely/aop/LogMethod.java
+++ src/main/java/com/efedorchenko/timely/aop/LogMethod.java
@@ -1,12 +1,14 @@
 package com.efedorchenko.timely.aop;
 
-import com.efedorchenko.timely.filter.RqUIDFilter;
+import com.efedorchenko.timely.filter.RequestUidFilter;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.slf4j.Marker;
 import org.slf4j.MarkerFactory;
 import org.springframework.stereotype.Component;
@@ -16,10 +18,13 @@ import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-@Slf4j
 @Aspect
 @Component
 public class LogMethod {
+
+    private final String INPUT_PATTERN =  "-> ({})";
+    private final String OUTPUT_PATTERN = "<- ({})";
+    private final String EX_PATTERN =     "!- {}";
 
     @Pointcut("execution(* com.efedorchenko.timely.controller.AuthController.*(..))")
     public void pointcutEndpoint() {
@@ -30,33 +35,35 @@ public class LogMethod {
         try {
 
             return Mono.deferContextual(contextView -> {
-                String rquid = contextView.getOrDefault(RqUIDFilter.RQUID, "NO_RqUID");
+                String rquid = contextView.getOrDefault(RequestUidFilter.RQUID, "NO_RqUID");
                 Marker rquidMarker = MarkerFactory.getMarker(rquid);
 
                 Method method = ((MethodSignature) joinPoint.getSignature()).getMethod();
                 String params = Arrays.stream(joinPoint.getArgs()).map(Object::toString).collect(Collectors.joining(", "));
-                log.info(rquidMarker, "-> [{}.{}] - ({})", method.getDeclaringClass().getSimpleName(), method.getName(), params);
+                Logger log = LoggerFactory.getLogger(method.getDeclaringClass().getName() + "." + method.getName());
+
+                log.info(rquidMarker, INPUT_PATTERN, params);
 
                 Object result;
                 try {
                     result = joinPoint.proceed();
                 } catch (Throwable throwable) {
-                    log.error(rquidMarker, "!- {}", throwable.getMessage());
+                    log.error(rquidMarker, EX_PATTERN, throwable.getMessage());
                     throw new ReactiveLogException(throwable);
                 }
 
                 if (result instanceof Mono<?> mono) {
 
                     return mono.flatMap(value -> {
-                        log.info(rquidMarker, "<- ({})", value.toString());
+                        log.info(rquidMarker, OUTPUT_PATTERN, value.toString());
                         return Mono.just(value);
                     }).onErrorResume(ex -> {
-                        log.error(rquidMarker, "<- ({})", ex.getMessage());
+                        log.error(rquidMarker, OUTPUT_PATTERN, ex.getMessage());
                         return Mono.error(ex);
                     });
 
                 } else {
-                    log.info(rquidMarker, "<- ({})", result);
+                    log.info(rquidMarker, OUTPUT_PATTERN, result);
                     return Mono.just(result);
                 }
             });
