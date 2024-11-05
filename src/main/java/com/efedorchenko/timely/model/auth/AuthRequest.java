5e82aec1817b7d5296f25eb30bd5f161a4456067
evgeniyf <jecky432@gmail.com>
2024-11-05 21:55:10 +0500

    data: added validation to auth (register) request dto

--- src/main/java/com/efedorchenko/timely/model/auth/AuthRequest.java
+++ src/main/java/com/efedorchenko/timely/model/auth/AuthRequest.java
@@ -1,6 +1,9 @@
 package com.efedorchenko.timely.model.auth;
 
+import com.efedorchenko.timely.model.validation.Constant;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.Setter;
@@ -10,11 +13,13 @@ import lombok.ToString;
 @Setter(AccessLevel.NONE)
 public class AuthRequest {
 
+    @Email
     @NotBlank
     private final String username;
 
     @NotBlank
     @ToString.Exclude
+    @Pattern(regexp = Constant.PASSWORD_REGEX)
     private final String password;
 
 }
--- src/main/java/com/efedorchenko/timely/model/auth/RegisterRequest.java
+++ src/main/java/com/efedorchenko/timely/model/auth/RegisterRequest.java
@@ -1,7 +1,10 @@
 package com.efedorchenko.timely.model.auth;
 
+import com.efedorchenko.timely.model.validation.Constant;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -15,11 +18,13 @@ import java.util.List;
 @AllArgsConstructor
 public class RegisterRequest {
 
+    @Email
     @NotBlank
     private final String username;
 
     @NotBlank
     @ToString.Exclude
+    @Pattern(regexp = Constant.PASSWORD_REGEX)
     private final String password;
 
     @NotNull
