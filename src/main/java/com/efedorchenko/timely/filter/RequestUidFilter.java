1e3763d08fe7665a9175032df097e928db0d4e28
evgeniyf <jecky432@gmail.com>
2024-11-10 22:13:05 +0500

    reactive-rejection: RequestUidFilter

--- src/main/java/com/efedorchenko/timely/filter/RequestUidFilter.java
+++ src/main/java/com/efedorchenko/timely/filter/RequestUidFilter.java
@@ -1,44 +1,54 @@
 package com.efedorchenko.timely.filter;
 
-import org.springframework.core.io.buffer.DataBuffer;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.server.reactive.ServerHttpResponse;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.MDC;
+import org.springframework.core.Ordered;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
-import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.server.WebFilter;
-import org.springframework.web.server.WebFilterChain;
-import reactor.core.publisher.Mono;
-import reactor.util.annotation.NonNull;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.nio.charset.StandardCharsets;
+import java.io.IOException;
 import java.util.regex.Pattern;
 
 @Component
-public class RequestUidFilter implements WebFilter {
+public class RequestUidFilter extends OncePerRequestFilter implements Ordered {
 
     public static final String RQUID = "RqUID";
-    private static final Pattern RQUID_PATTERN = Pattern.compile("^[a-zA-Z\\d]{20,40}$");
+    private static final Pattern RQUID_PATTERN = Pattern.compile("^[a-zA-Z\\d-]{20,40}$");
 
-    @NonNull
     @Override
-    public Mono<Void> filter(@NonNull ServerWebExchange exchange, @NonNull WebFilterChain chain) {
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
 
-        String rquid = exchange.getRequest().getHeaders().getFirst(RQUID);
-        ServerHttpResponse response = exchange.getResponse();
+        String rquid = request.getHeader(RQUID);
 
-        if (rquid == null || rquid.isEmpty() || RQUID_PATTERN.matcher(rquid).matches()) {
-            return badRequest(response);
+        if (rquid == null || rquid.isEmpty() || !RQUID_PATTERN.matcher(rquid).matches()) {
+            badRequest(response);
+            return;
         }
-        response.getHeaders().set(RQUID, rquid);
 
-        return chain.filter(exchange)
-                .contextWrite(ctx -> ctx.put(RQUID, rquid));
+        try {
+            MDC.put(RQUID, rquid);
+            response.setHeader(RQUID, rquid);
+            filterChain.doFilter(request, response);
+
+        } finally {
+            MDC.clear();
+        }
     }
 
-    private Mono<Void> badRequest(ServerHttpResponse response) {
-        response.setStatusCode(HttpStatus.BAD_REQUEST);
+    private void badRequest(HttpServletResponse response) throws IOException {
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
         String responseBody = RQUID + " is required and must be matches " + RQUID_PATTERN.pattern();
-        DataBuffer buffer = response.bufferFactory().wrap(responseBody.getBytes(StandardCharsets.UTF_8));
-        return response.writeWith(Mono.just(buffer));
+        response.getWriter().write(responseBody);
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.HIGHEST_PRECEDENCE;
     }
 }
