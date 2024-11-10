c380d3bdde0f35ba48a9d509ee16b6eeb6182270
evgeniyf <jecky432@gmail.com>
2024-11-10 22:15:20 +0500

    reactive-rejection-data: controller

--- src/main/java/com/efedorchenko/timely/controller/DataController.java
+++ src/main/java/com/efedorchenko/timely/controller/DataController.java
@@ -1,26 +1,11 @@
 package com.efedorchenko.timely.controller;
 
 import com.efedorchenko.timely.entity.UserData;
-import com.efedorchenko.timely.logging.Log;
-import com.efedorchenko.timely.model.DataRangeRequest;
-import com.efedorchenko.timely.model.EventsAndFines;
-import com.efedorchenko.timely.model.UserDataType;
 import com.efedorchenko.timely.service.UserDataService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-
-import java.util.UUID;
 
 @RequiredArgsConstructor
 @RestController
@@ -34,37 +19,37 @@ public class DataController {
 
     private final UserDataService<UserData> userDataService;
 
-    @Log
-    @PostMapping
-    public Mono<ResponseEntity<Void>> addData(@AuthenticationPrincipal UUID userId,
-                                              @RequestBody @Valid UserData userData) {
-        return userDataService.addData(userId, userData)
-                .thenReturn(ResponseEntity.accepted().build());
-    }
-
-    @Log
-    @DeleteMapping
-    public Mono<ResponseEntity<Void>> removeData(@AuthenticationPrincipal UUID userId,
-                                                 @RequestBody @Valid UserData userData) {
-        return userDataService.removeData(userId, userData)
-                .thenReturn(ResponseEntity.accepted().build());
-    }
-
-    @Log
-    @PostMapping(path = "/range")
-    public Flux<?> getRange(@AuthenticationPrincipal UUID userId,
-                            @RequestParam UserDataType type,
-                            @RequestBody @Valid DataRangeRequest dataRangeRequest) {
-        return userDataService.getRange(userId, dataRangeRequest, type)
-                .map(r -> ResponseEntity.ok().body(r));
-    }
-
-    @Log
-    @PostMapping(path = "/range-all")
-    public Mono<ResponseEntity<EventsAndFines>> getRangeAllTypes(
-            @AuthenticationPrincipal UUID userId,
-            @RequestBody @Valid DataRangeRequest dataRangeRequest) {
-        return userDataService.getRange(userId, dataRangeRequest)
-                .map(r -> ResponseEntity.ok().body(r));
-    }
+//    @Log
+//    @PostMapping
+//    public Mono<ResponseEntity<Void>> addData(@AuthenticationPrincipal UUID userId,
+//                                              @RequestBody @Valid UserData userData) {
+//        return userDataService.addData(userId, userData)
+//                .thenReturn(ResponseEntity.accepted().build());
+//    }
+//
+//    @Log
+//    @DeleteMapping
+//    public Mono<ResponseEntity<Void>> removeData(@AuthenticationPrincipal UUID userId,
+//                                                 @RequestBody @Valid UserData userData) {
+//        return userDataService.removeData(userId, userData)
+//                .thenReturn(ResponseEntity.accepted().build());
+//    }
+//
+//    @Log
+//    @PostMapping(path = "/range")
+//    public Flux<?> getRange(@AuthenticationPrincipal UUID userId,
+//                            @RequestParam UserDataType type,
+//                            @RequestBody @Valid DataRangeRequest dataRangeRequest) {
+//        return userDataService.getRange(userId, dataRangeRequest, type)
+//                .map(r -> ResponseEntity.ok().body(r));
+//    }
+//
+//    @Log
+//    @PostMapping(path = "/range-all")
+//    public Mono<ResponseEntity<EventsAndFines>> getRangeAllTypes(
+//            @AuthenticationPrincipal UUID userId,
+//            @RequestBody @Valid DataRangeRequest dataRangeRequest) {
+//        return userDataService.getRange(userId, dataRangeRequest)
+//                .map(r -> ResponseEntity.ok().body(r));
+//    }
 }
--- src/main/java/com/efedorchenko/timely/service/UserDataService.java
+++ src/main/java/com/efedorchenko/timely/service/UserDataService.java
@@ -3,18 +3,16 @@ package com.efedorchenko.timely.service;
 import com.efedorchenko.timely.model.DataRangeRequest;
 import com.efedorchenko.timely.model.EventsAndFines;
 import com.efedorchenko.timely.model.UserDataType;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
 public interface UserDataService<T> {
 
-    Mono<Void> addData(UUID userId, T event);
+    Void addData(UUID userId, T event);
 
-    Mono<Void> removeData(UUID userId, T userData);
+    Void removeData(UUID userId, T userData);
 
-    Flux<T> getRange(UUID userId, DataRangeRequest dataRangeRequest, UserDataType dataType);
+    T getRange(UUID userId, DataRangeRequest dataRangeRequest, UserDataType dataType);
 
-    Mono<EventsAndFines> getRange(UUID userId, DataRangeRequest dataRangeRequest);
+    EventsAndFines getRange(UUID userId, DataRangeRequest dataRangeRequest);
 }
