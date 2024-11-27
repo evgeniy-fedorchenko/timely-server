5e6167834bba69986dd016cc81361f28ccb490c3
evgeniyf <jecky432@gmail.com>
2024-11-28 00:58:06 +0500

    data: config: swagger

---
+++ src/main/java/com/efedorchenko/timely/configuration/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.efedorchenko.timely.configuration;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("dev")
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi(@Value("${app.version}") String applicationVersion,
+                                    @Value("${spring.application.name}") String applicationName) {
+        return GroupedOpenApi.builder()
+                .group(applicationName + " " + applicationVersion)
+                .pathsToMatch(ApplicationProperties.BASE_PATH + "/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi actuatorApi() {
+        return GroupedOpenApi.builder().group("actuator").pathsToMatch("/actuator/**").build();
+    }
+
+}
