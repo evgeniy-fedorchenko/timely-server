4b743a3ec29fd60981ec41e0b710fa9712b442a2
evgeniyf <jecky432@gmail.com>
2024-11-30 22:17:49 +0500

    data: refactor controller

--- src/main/java/com/efedorchenko/timely/controller/AuthController.java
+++ src/main/java/com/efedorchenko/timely/controller/AuthController.java
@@ -1,56 +1,64 @@
 package com.efedorchenko.timely.controller;
 
+import com.efedorchenko.timely.configuration.ApplicationProperties;
+import com.efedorchenko.timely.logging.Level;
 import com.efedorchenko.timely.logging.Log;
+import com.efedorchenko.timely.model.auth.AuthFailReason;
 import com.efedorchenko.timely.model.auth.AuthResponse;
-import com.efedorchenko.timely.model.auth.JwtTokenData;
 import com.efedorchenko.timely.model.auth.RegisterRequest;
-import com.efedorchenko.timely.security.JwtUtil;
 import com.efedorchenko.timely.service.AuthService;
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
 import java.util.UUID;
 
-@Slf4j
-@RequiredArgsConstructor
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@Log(Level.DEBUG)
+@Validated
+@AllArgsConstructor
 @RestController
-@RequestMapping(path = AuthController.AUTH_ENDPOINT, produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(path = AuthController.AUTH_ENDPOINT, produces = APPLICATION_JSON_VALUE)
 public class AuthController {
 
-    public static final String AUTH_ENDPOINT = "/auth";
+    public static final String AUTH_ENDPOINT = ApplicationProperties.BASE_PATH + "/auth";
 
-    private final JwtUtil jwtUtil;
-    private final AuthService authService;
+    private final AuthService<RegisterRequest, AuthResponse> authService;
 
-    @Log
     @PostMapping(path = "/login")
     public ResponseEntity<AuthResponse> login(@AuthenticationPrincipal UUID userId) {
-        JwtTokenData jwtTokenData = authService.login(userId);
-        return ResponseEntity.ok(generateAuthResponse(jwtTokenData));
+        return computeResponseEntity(authService.login(userId));
     }
 
-    @Log
     @GetMapping(path = "/logout")
     public ResponseEntity<Void> logout() {
-        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).build(); // TODO 02.11.2024 22:49: реализовать logout
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).build();
     }
 
-    @Log
-    @PostMapping(path = "/reg")
+    @Log(Level.INFO)
+    @PostMapping(path = "/reg", consumes = APPLICATION_JSON_VALUE)
     public ResponseEntity<AuthResponse> register(@RequestBody @Valid RegisterRequest registerRequest) {
-        JwtTokenData jwtTokenData = authService.register(registerRequest);
-        return ResponseEntity.ok(generateAuthResponse(jwtTokenData));
+        return computeResponseEntity(authService.register(registerRequest));
     }
 
-    private AuthResponse generateAuthResponse(JwtTokenData token) {
-        return token.getUserId() == null
-                ? AuthResponse.failWith(token.getAuthFailReason())
-                : AuthResponse.success(jwtUtil.generateToken(token), token.getAuthorities());
+    private ResponseEntity<AuthResponse> computeResponseEntity(AuthResponse response) {
+        if (response.isRegister()) {
+            return ResponseEntity.ok(response);
+        } else {
+            HttpStatus status = Optional.ofNullable(response.getReason())
+                    .map(AuthFailReason::getHttpStatus)
+                    .orElse(HttpStatus.BAD_REQUEST);
+            return ResponseEntity.status(status).body(response);
+        }
     }
 }
