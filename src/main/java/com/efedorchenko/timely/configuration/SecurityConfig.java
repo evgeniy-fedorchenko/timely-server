62a7e9c7572725cd71d5d5f8484de7674662e36f
evgeniyf <jecky432@gmail.com>
2024-11-30 22:18:29 +0500

    data: enable method security for preAuthorise

--- src/main/java/com/efedorchenko/timely/configuration/SecurityConfig.java
+++ src/main/java/com/efedorchenko/timely/configuration/SecurityConfig.java
@@ -8,6 +8,7 @@ import com.efedorchenko.timely.security.LoginAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -23,6 +24,7 @@ import static com.efedorchenko.timely.configuration.ApplicationProperties.BASE_P
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
