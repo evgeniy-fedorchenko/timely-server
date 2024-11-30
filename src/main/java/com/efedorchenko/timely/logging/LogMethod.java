d8491ba4c6c784760acafef89dcabb27cbda775d
evgeniyf <jecky432@gmail.com>
2024-11-30 21:55:32 +0500

    data: add LogSupport and use all @Log-settings in aspect

--- src/main/java/com/efedorchenko/timely/logging/LogMethod.java
+++ src/main/java/com/efedorchenko/timely/logging/LogMethod.java
@@ -1,6 +1,7 @@
 package com.efedorchenko.timely.logging;
 
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -8,18 +9,27 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.event.Level;
 import org.springframework.stereotype.Component;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+import static com.efedorchenko.timely.logging.Log.Ignore;
+import static com.efedorchenko.timely.logging.Log.IgnoreAll;
+
+@Slf4j
 @Aspect
 @Component
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class LogMethod {
 
     private static final String INPUT_PATTERN = "-> (%s)";
@@ -28,59 +38,113 @@ public class LogMethod {
 
     private final ExecutorService executorOfVirtual;
 
-    @Pointcut("@annotation(logAnnotation)")
-    public void aroundLogPointcut(Log logAnnotation) {
+    @Pointcut("@annotation(logAnnotation) || @within(logAnnotation)")
+    public void logPointcut(Log logAnnotation) {
     }
 
-    @Around(value = "aroundLogPointcut(logAnnotation)", argNames = "joinPoint,logAnnotation")
-    public Object aroundLog(ProceedingJoinPoint joinPoint, Log logAnnotation) throws Throwable {
+    @Around(value = "logPointcut(logAnnotation)", argNames = "joinPoint,logAnnotation")
+    public Object logAround(ProceedingJoinPoint joinPoint, Log logAnnotation) throws Throwable {
 
         Method method = ((MethodSignature) joinPoint.getSignature()).getMethod();
-        Logger logger = LoggerFactory.getLogger(method.getDeclaringClass().getName() + "." + method.getName());
 
-        Level level = logAnnotation.level();
-        boolean enabledForLevel = logger.isEnabledForLevel(level);
-
-        if (enabledForLevel) {
-
-            CompletableFuture.runAsync(() -> {
-
-                String params = Arrays.stream(joinPoint.getArgs())
-                        .map(Object::toString)
-                        .collect(Collectors.joining(", "));
-
-                logger.atLevel(level).log(INPUT_PATTERN.formatted(params));
-
-            }, executorOfVirtual);
+        if (method.isAnnotationPresent(IgnoreAll.class)) {
+            return joinPoint.proceed();
         }
 
+        Log methodAnnotation = method.getAnnotation(Log.class);
+        if (methodAnnotation != null) {
+            logAnnotation = methodAnnotation;
+        }
+
+        LogSupport logSupport = new LogSupport(logAnnotation, method.getAnnotation(Ignore.class));
+        Logger logger = LoggerFactory.getLogger(method.getDeclaringClass().getName() + "." + method.getName());
+        boolean enabledForLevel = logger.isEnabledForLevel(logSupport.getArgsLevel());
+
+        if (enabledForLevel && !logSupport.needsIgnoreInvoke()) {
+            CompletableFuture.runAsync(
+                    () -> logArguments(joinPoint.getArgs(), method.getParameters(), logger, logSupport),
+                    executorOfVirtual
+            );
+        }
+
+        Object result;
         try {
-            Object result = joinPoint.proceed();
-                if (result instanceof CompletableFuture<?> future) {
-                    future.whenComplete((res, ex) -> {
-                        if (ex != null) {
-                            String mess = EX_PATTERN.formatted(ex.toString());
-                            logger.error(mess);
-                        } else {
-                            if (enabledForLevel) {
-                                String mess = OUTPUT_PATTERN.formatted(res.toString());
-                                logger.atLevel(level).log(mess);
-                            }
+            result = joinPoint.proceed();
+        } catch (Throwable t) {
+            doLogResult(t, logger, logSupport);
+            throw t;
+        }
+
+
+        if (result instanceof CompletableFuture<?> future) {
+            future.whenComplete((res, ex) ->
+                    doLogResult((ex != null ? ex : res), logger, logSupport));
+
+        } else if (method.getReturnType().equals(Void.TYPE)) {
+            doLogResult("", logger, logSupport);
+
+        } else {
+            doLogResult(result, logger, logSupport);
+        }
+
+        return result;
+    }
+
+    private void logArguments(Object[] args, Parameter[] sourceParams, Logger logger, LogSupport logSupport) {
+        try {
+
+            if (logSupport.needsIgnoreArguments()) {
+                logger.atLevel(logSupport.getArgsLevel()).log(INPUT_PATTERN.formatted(""));
+                return;
+            }
+
+
+            List<Object> loggableArgs = new ArrayList<>();
+            IntStream.range(0, args.length).forEach(idx -> {
+                        Optional<Annotation> first = Arrays.stream(sourceParams[idx].getAnnotations())
+                                .filter(a -> a.annotationType().equals(Ignore.class))
+                                .findFirst();
+                        if (first.isEmpty()) {
+                            loggableArgs.add(args[idx]);
                         }
-                    });
-                } else {
-                    if (enabledForLevel) {
-                        logger.atLevel(level).log(OUTPUT_PATTERN.formatted(result.toString()));
                     }
+            );
+            String params = loggableArgs.isEmpty()
+                    ? ""
+                    : loggableArgs.stream()
+                    .map(Object::toString)
+                    .collect(Collectors.joining(", "));
+
+            logger.atLevel(logSupport.getArgsLevel()).log(INPUT_PATTERN.formatted(params));
+        } catch (Throwable t) {
+            log.error("Cannot logging parameters of method [{}]. Ex: {}", logger.getName(), t.getMessage());
+        }
+    }
+
+    private void doLogResult(Object loggingObj, Logger logger, LogSupport logSupport) {
+        try {
+
+            if (loggingObj instanceof Throwable t
+                    && logger.isEnabledForLevel(logSupport.getExLevel())
+                    && logSupport.isLoggableEx(t)) {
+
+                String mess = EX_PATTERN.formatted(t.toString());
+                logger.atLevel(logSupport.getExLevel()).log(mess);
+
+            } else if (logger.isEnabledForLevel(logSupport.getExLevel())) {
+                if (logSupport.needsIgnoreCompletion()) {
+                    return;
+
+                } else if (logSupport.needsIgnoreReturnedValue()) {
+                    logger.atLevel(logSupport.getReturnLevel()).log(OUTPUT_PATTERN.formatted(""));
+                    return;
                 }
+                String mess = OUTPUT_PATTERN.formatted(loggingObj != null ? loggingObj.toString() : "null");
+                logger.atLevel(logSupport.getReturnLevel()).log(mess);
+            }
 
-            return result;
-
-        } catch (Throwable ex) {
-            String mess = EX_PATTERN.formatted(ex.toString());
-            logger.atLevel(level).log(mess);
-
-            throw ex;
+        } catch (Throwable t) {
+            log.error("Cannot logging result of method [{}]. Ex: {}", logger.getName(), t.getMessage());
         }
     }
 }
