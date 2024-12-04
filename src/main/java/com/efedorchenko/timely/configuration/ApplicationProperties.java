a58f0fdfc934354556cc062984794867629d3012
evgeniyf <jecky432@gmail.com>
2024-12-04 20:46:42 +0500

    data: add ehcache

--- src/main/java/com/efedorchenko/timely/configuration/ApplicationProperties.java
+++ src/main/java/com/efedorchenko/timely/configuration/ApplicationProperties.java
@@ -8,4 +8,6 @@ public class ApplicationProperties {
 
     public static final String RQUID = "RqUID";
     public static final Pattern RQUID_PATTERN = Pattern.compile("^[a-zA-Z\\d-]{20,40}$");
+
+    public static final String ROLES_CACHE_NAME = "roles";
 }
--- src/main/java/com/efedorchenko/timely/repository/RoleRepository.java
+++ src/main/java/com/efedorchenko/timely/repository/RoleRepository.java
@@ -1,20 +1,16 @@
 package com.efedorchenko.timely.repository;
 
-import com.efedorchenko.timely.configuration.CacheConfig;
+import com.efedorchenko.timely.configuration.ApplicationProperties;
 import com.efedorchenko.timely.entity.Role;
 import com.efedorchenko.timely.model.auth.RoleType;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
-import java.util.Set;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
-    @Cacheable(cacheNames = CacheConfig.ROLES_CACHE_NAME, unless = "#result == null")
+    @Cacheable(cacheNames = ApplicationProperties.ROLES_CACHE_NAME, unless = "#result == null")
     Optional<Role> findByValue(RoleType value);
 
-    @Query("SELECT r.value FROM Role r")
-    Set<RoleType> getValues();
 }
