bc358e00551db799111bf33d5b8fcb297d7b731f
evgeniyf <jecky432@gmail.com>
2024-12-04 19:48:51 +0500

    data: add requestedUserId to rangeRequest

--- src/main/java/com/efedorchenko/timely/model/data/DataRangeRequest.java
+++ src/main/java/com/efedorchenko/timely/model/data/DataRangeRequest.java
@@ -1,12 +1,14 @@
 package com.efedorchenko.timely.model.data;
 
 import com.efedorchenko.timely.model.validation.CurrentDatesRange;
+import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 
 import java.time.YearMonth;
+import java.util.UUID;
 
 @Getter
 @ToString
@@ -19,5 +21,8 @@ public class DataRangeRequest {
 
     @NotNull
     private final YearMonth end;
+
+    @Nullable
+    private final UUID requestedUserId; 
 
 }
