9a90b0a7ded99c48bbbe81a1bb57835ba1fec8ee
evgeniyf <jecky432@gmail.com>
2025-01-08 03:27:38 +0500

    release-1.0.0: deleted unnecessary fields from fineDto

--- src/main/java/com/efedorchenko/timely/model/data/FineDto.java
+++ src/main/java/com/efedorchenko/timely/model/data/FineDto.java
@@ -3,16 +3,12 @@ package com.efedorchenko.timely.model.data;
 import com.efedorchenko.timely.model.validation.Constant;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.annotation.Nullable;
-import jakarta.validation.constraints.Future;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 import lombok.extern.jackson.Jacksonized;
-
-import java.time.LocalDate;
 
 @Getter
 @ToString
@@ -22,14 +18,6 @@ import java.time.LocalDate;
 public class FineDto extends UserDataDto {
 
     static final String TYPE = "fine";
-
-    @Positive
-    @Nullable
-    private final Long id;
-
-    @Future
-    @NotNull
-    private final LocalDate date;
 
     @Nullable
     @Size(max = Constant.FINE_DESCRIPTION_MAX_LEN)
